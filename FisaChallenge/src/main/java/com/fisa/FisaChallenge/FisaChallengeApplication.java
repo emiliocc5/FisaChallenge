e22d38830e2ecb57c627d35ccc5e96362fa5c716
emiliocc5 <emiliocaccia@live.com.ar>
2021-01-29 19:35:26 -0300

    Initial commit

---
+++ FisaChallenge/src/main/java/com/fisa/FisaChallenge/FisaChallengeApplication.java
@@ -0,0 +1,13 @@
+package com.fisa.FisaChallenge;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FisaChallengeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FisaChallengeApplication.class, args);
+	}
+
+}
---
+++ FisaChallenge/src/test/java/com/fisa/FisaChallenge/FisaChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fisa.FisaChallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FisaChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
